91ef2075a7b67752f30217e1cbc41d1a2fa10162
Neveta0905 <lulued@free.fr>
2022-01-19 22:43:24 +0100

    regex

--- front/Js/Products.js
+++ front/Js/Products.js
@@ -1,28 +1,139 @@
 // General
-// ----- Index -----
-// Create Item in index page
-function create_item_index(id,imgurl,altTxt,prod_name,prod_descript){
-	let new_item = Create_HTML_elem('a',{'href':'./product.html?id='+id})
-	new_item.appendChild(document.createElement('article'))
-	
-	let image = Create_HTML_elem('img',{'src':imgurl,'alt':altTxt})
-	let h3 = Create_HTML_elem('h3',{'class':'productName'},prod_name)
-	let para = Create_HTML_elem('p',{'class':'productDescription'},prod_descript)
+// Classes 
+// ----- Cart ----
+class Cart{
+	static get_cart(){ // return array[ [id,color,qty], [id2,color2,qty2] ]
+		let cart_ordered = []
 
+		for(let[key,value] of Object.entries(window.sessionStorage)){
+			let article = key + " " + value
+			let splited = article.split(' ')
+			cart_ordered.push(splited)
+		}
+		return cart_ordered
+	}
 
-	new_item.children[0].appendChild(image)
-	new_item.children[0].appendChild(h3)
-	new_item.children[0].appendChild(para)
+	static add_product(){
+		let asked_color = document.getElementById('colors').value
+		let product_id = get_param('id')
 
-	var elem_in = document.getElementById('items');
-	var elem_after = elem_in.lastChild;
+		// Créer une clef couleur-id du produit
+		let color_name_key = product_id + ' ' + asked_color
+		let qty = document.getElementById('quantity').value
+		let qty_int = parseInt(qty)
+		let already_asked_qty = parseInt(sessionStorage.getItem(color_name_key))
 
-	elem_in.insertBefore(new_item, elem_after);
+		Cart.check_product_added(asked_color)
+		if(qty > 0 && asked_color != ''){
+			sessionStorage.getItem(color_name_key) == null ?
+			sessionStorage.setItem(color_name_key,qty) :
+			sessionStorage.setItem(color_name_key,already_asked_qty + qty_int)
+		}
+	}
+
+	static remove_product(id,color){
+		let clef = id + ' ' + color
+		sessionStorage.removeItem(clef)
+		window.location.href = ''
+	}
+
+	static check_product_added(color){
+		let prod_name = document.getElementById('title').innerHTML
+		let redirect
+		color != '' ? redirect = window.confirm('Votre produit ' + prod_name + ' de couleur ' + color.toLowerCase() + ' est ajouté à votre panier. Souhaitez vous retourner à la page d\'accueil ?'):
+		alert('N\'oubliez pas de choisir une couleur')
+		
+		if(redirect) 
+			window.location.href= 'index.html' //Renvoie à l'accueil
+	}
+
+	static getQties(get_cart){
+		let qty_arr = []
+
+		for(let elem of get_cart){
+			qty_arr.push(elem[2])
+		}
+		return qty_arr
+	}
+
+	static Modify_qty(id,color,new_nbre){
+		let clef = id + ' ' + color
+		sessionStorage.setItem(clef,new_nbre)
+		
+		let cart_article = document.getElementsByClassName('cart__item__content')
+
+		for(let i=0;i<cart_article.length;i++){
+			let price = cart_article[i].getElementsByClassName('cart__item__content__description')[0].children[2].innerHTML
+			price = price.replace('€','')
+			price = price.replace(',','.')
+
+			let qty = cart_article[i].getElementsByClassName('itemQuantity')[0].value
+			
+
+			let price_layer = cart_article[i].getElementsByClassName('article_total_price')[0]
+			price_layer.innerHTML = 'Prix des produits : ' + addCommas(price * qty) + ' €';
+		}
+	}
+
+	static Totalqties(arr_qties){
+		let price =0
+		for(let elem of arr_qties){
+			price += parseInt(elem,10)
+		}
+		return price
+	}
+
+	static getPrices(get_cart){
+		return new Promise((next)=>{
+			let prices = []
+
+			for(let elem of get_cart){
+				let my_furniture = new Item
+				my_furniture.getPrice(elem[0],(price)=>{
+					prices.push(price)
+
+					if(get_cart.length === prices.length){
+						next(prices)
+					}
+				})
+			}
+		})
+	}
+
+	static show(cart){
+		for (let elem of cart){
+		//Elem infos
+			let elem_id = elem[0]
+			let elem_color = elem[1]
+			let elem_qty = elem[2]
+
+			let article = new Item;
+
+			(async function call_id(){
+				let article_to_lay = await article.getById(elem_id)
+				article_to_lay.Create_cart_article(elem_id,elem_color,elem_qty)
+			})()
+		}
+	}
+
+	static CalculateTotalPrice(qty_arr,prices_arr){
+		let total_price=0
+		for(let i = 0;i<qty_arr.length;i++){
+			let total_prod_amount = parseInt(qty_arr[i], 10) * prices_arr[i]
+			total_price += total_prod_amount
+		}
+		return (total_price)
+	}
+
+	static lay_totals(total_price,total_qties){
+		let price_zone = document.getElementById('totalPrice')
+		let qty_zone = document.getElementById('totalQuantity')
+
+		price_zone.innerHTML = addCommas(total_price)
+		qty_zone.innerHTML = total_qties
+	}
 }
 
-
-
-// ----- Individual Product menu -----
 class Item{
 	constructor(colors,id,name,price,imageURL,description,altTxt){
 		this.colors = colors
@@ -201,6 +312,87 @@ class Item{
 	}
 }
 
+class Contact{
+	constructor(prénom,nom,adresse,ville,email){
+		this.prénom = prénom
+		this.nom = nom
+		this.adresse = adresse
+		this.ville = ville
+		this.email = email
+		this.string_rules = new Regexs
+		this.checkRules()
+		this.selfdestroy()
+	}
+
+	checkRules(){
+		let pr =  this.string_rules.prénom.test(this.prénom)
+		let nm = this.string_rules.nom.test(this.nom)
+		let ad = this.string_rules.adresse.test(this.adresse)
+		let vi = this.string_rules.ville.test(this.ville)
+		let em = this.string_rules.email.test(this.email)
+		let error_mess= ''
+		
+		if(!pr)
+			error_mess = error_mess + 'Prénom non conforme \n'
+		if(!nm)
+			error_mess = error_mess + 'Nom non conforme \n'
+		if(!ad)
+			error_mess = error_mess + 'Adresse non conforme \n'
+		if(!vi)
+			error_mess = error_mess + 'Ville non conforme \n'
+		if(!em)
+			error_mess = error_mess + 'Mail non conforme \n'
+
+		if(pr === false || nm === false ||ad === false || vi === false || em === false){
+			this.selfdestroy()
+			//window.location.replace(window.location.href)
+			alert(error_mess)
+			error_mess=''
+		}
+	}
+
+	selfdestroy(){
+		for(let[key,value] of Object.entries(this)){
+			delete this[key]
+		}
+	}
+}
+
+class Regexs{
+	constructor(prénom,nom,adresse,ville,email){
+		this.prénom = new RegExp("^[a-zA-Z._-]{3,50}")
+		this.nom = new RegExp("^[0-9a-zA-Z._-]{3,50}")
+		this.adresse = new RegExp("[0-9]*[a-zA-Z]*")
+		this.ville = new RegExp("[a-zA-Z]")
+		this.email = new RegExp("[a-z0-9\-_]+[a-z0-9\.\-_]*@[a-z0-9\-_]{2,}\.[a-z\.\-_]+[a-z\-_]+")
+	}
+}
+
+
+let conta = new Contact('1lulu','edm','8 P Gd','Cassel','luluedfree.fr')
+console.log(conta)
+
+// ----- Index -----
+// Create Item in index page
+function create_item_index(id,imgurl,altTxt,prod_name,prod_descript){
+	let new_item = Create_HTML_elem('a',{'href':'./product.html?id='+id})
+	new_item.appendChild(document.createElement('article'))
+	
+	let image = Create_HTML_elem('img',{'src':imgurl,'alt':altTxt})
+	let h3 = Create_HTML_elem('h3',{'class':'productName'},prod_name)
+	let para = Create_HTML_elem('p',{'class':'productDescription'},prod_descript)
+
+
+	new_item.children[0].appendChild(image)
+	new_item.children[0].appendChild(h3)
+	new_item.children[0].appendChild(para)
+
+	var elem_in = document.getElementById('items');
+	var elem_after = elem_in.lastChild;
+
+	elem_in.insertBefore(new_item, elem_after);
+}
+
 // Create all items in index
 async function Create_items(api_name){
 	let donnees = await Item.getAll() // donnees est le retour de promesse
@@ -209,139 +401,6 @@ async function Create_items(api_name){
 	}
 }
 
-// ----- Cart ----
-class Cart{
-	static get_cart(){ // return array[ [id,color,qty], [id2,color2,qty2] ]
-		let cart_ordered = []
-
-		for(let[key,value] of Object.entries(window.sessionStorage)){
-			let article = key + " " + value
-			let splited = article.split(' ')
-			cart_ordered.push(splited)
-		}
-		return cart_ordered
-	}
-
-	static add_product(){
-		let asked_color = document.getElementById('colors').value
-		let product_id = get_param('id')
-
-		// Créer une clef couleur-id du produit
-		let color_name_key = product_id + ' ' + asked_color
-		let qty = document.getElementById('quantity').value
-		let qty_int = parseInt(qty)
-		let already_asked_qty = parseInt(sessionStorage.getItem(color_name_key))
-
-		Cart.check_product_added(asked_color)
-		if(qty > 0 && asked_color != ''){
-			sessionStorage.getItem(color_name_key) == null ?
-			sessionStorage.setItem(color_name_key,qty) :
-			sessionStorage.setItem(color_name_key,already_asked_qty + qty_int)
-		}
-	}
-
-	static remove_product(id,color){
-		let clef = id + ' ' + color
-		sessionStorage.removeItem(clef)
-		window.location.href = ''
-	}
-
-	static check_product_added(color){
-		let prod_name = document.getElementById('title').innerHTML
-		let redirect
-		color != '' ? redirect = window.confirm('Votre produit ' + prod_name + ' de couleur ' + color.toLowerCase() + ' est ajouté à votre panier. Souhaitez vous retourner à la page d\'accueil ?'):
-		alert('N\'oubliez pas de choisir une couleur')
-		
-		if(redirect) 
-			window.location.href= 'index.html' //Renvoie à l'accueil
-	}
-
-	static getQties(get_cart){
-		let qty_arr = []
-
-		for(let elem of get_cart){
-			qty_arr.push(elem[2])
-		}
-		return qty_arr
-	}
-
-	static Modify_qty(id,color,new_nbre){
-		let clef = id + ' ' + color
-		sessionStorage.setItem(clef,new_nbre)
-		
-		let cart_article = document.getElementsByClassName('cart__item__content')
-
-		for(let i=0;i<cart_article.length;i++){
-			let price = cart_article[i].getElementsByClassName('cart__item__content__description')[0].children[2].innerHTML
-			price = price.replace('€','')
-			price = price.replace(',','.')
-
-			let qty = cart_article[i].getElementsByClassName('itemQuantity')[0].value
-			
-
-			let price_layer = cart_article[i].getElementsByClassName('article_total_price')[0]
-			price_layer.innerHTML = 'Prix des produits : ' + addCommas(price * qty) + ' €';
-		}
-	}
-
-	static Totalqties(arr_qties){
-		let price =0
-		for(let elem of arr_qties){
-			price += parseInt(elem,10)
-		}
-		return price
-	}
-
-	static getPrices(get_cart){
-		return new Promise((next)=>{
-			let prices = []
-
-			for(let elem of get_cart){
-				let my_furniture = new Item
-				my_furniture.getPrice(elem[0],(price)=>{
-					prices.push(price)
-
-					if(get_cart.length === prices.length){
-						next(prices)
-					}
-				})
-			}
-		})
-	}
-
-	static show(cart){
-		for (let elem of cart){
-		//Elem infos
-			let elem_id = elem[0]
-			let elem_color = elem[1]
-			let elem_qty = elem[2]
-
-			let article = new Item;
-
-			(async function call_id(){
-				let article_to_lay = await article.getById(elem_id)
-				article_to_lay.Create_cart_article(elem_id,elem_color,elem_qty)
-			})()
-		}
-	}
-
-	static CalculateTotalPrice(qty_arr,prices_arr){
-		let total_price=0
-		for(let i = 0;i<qty_arr.length;i++){
-			let total_prod_amount = parseInt(qty_arr[i], 10) * prices_arr[i]
-			total_price += total_prod_amount
-		}
-		return (total_price)
-	}
-
-	static lay_totals(total_price,total_qties){
-		let price_zone = document.getElementById('totalPrice')
-		let qty_zone = document.getElementById('totalQuantity')
-
-		price_zone.innerHTML = addCommas(total_price)
-		qty_zone.innerHTML = total_qties
-	}
-}
 
 const cart_price_caller = async (next) =>{
 	let total_prices = await Cart.getPrices(Cart.get_cart())
@@ -379,6 +438,9 @@ function Create_product(){ // Create individual Item menu
 		article_to_lay.Create_item_menu()
 	})
 }
+
+
+
 
 // Utils
 const Create_HTML_elem = (Type,Attributes,InnerHTML,Parent) => {
